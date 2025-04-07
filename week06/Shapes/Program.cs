af9625a0b8ffad16a6ef3c1a7f27d2d81ad6cc51
Anselem300 <Osamuel1@byupathway.edu>
2025-04-07 06:54:05 -0700

    Shapes Exercise completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine();
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Blue", 5));
+        shapes.Add(new Rectangle("Yellow", 5, 4));
+        shapes.Add(new Circle("Black", 3.7));
+
+        foreach (Shape items in shapes)
+        {
+            string color = items.GetColor();
+            double area = items.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
+        Console.WriteLine();
     }
 }
